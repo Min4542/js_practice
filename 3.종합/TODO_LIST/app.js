812b7a032ffda71ef64fe269f4876be8356a433e
Min4542 <looks454234@naver.com>
2021-07-22 11:56:40 +0900

    [21.07.22] js 수업 내용

--- 3.종합/TODO_LIST/app.js
+++ 3.종합/TODO_LIST/app.js
@@ -36,18 +36,23 @@ function makeNewId() {
 
 //새 할 일을 화면에 렌더링하는 함수
 function renderNewTodo(newTodo) {
+    const $li = document.createElement('li');
+    $li.dataset.id = newTodo.id;
+    $li.classList.add('todo-list-item');
+
     const newElement =
-        `<li data-id="${newTodo.id}" class="todo-list-item">
-<label class="checkbox">
+        `<label class="checkbox">
     <input type="checkbox">
     <span class="text">${newTodo.text}</span>
 </label>
 <div class="modify"><span class="lnr lnr-undo"></span></div>
 <div class="remove"><span class="lnr lnr-cross-circle"></span></div>
-</li>
+
 `;
+    $li.innerHTML = newElement;
+
     const $toDoList = document.querySelector('.todo-list');
-    $toDoList.innerHTML += newElement;
+    $toDoList.appendChild($li);
 
 };
 
@@ -70,24 +75,88 @@ function insertTodoData() {
     const $todoText = document.getElementById('todo-text');
 
     //입력값 검증 
-   
-    if (!validateInputText($todoText) ) return
-      //1.데이터 처리: 필요한 데이터를 생성하여 todos배열에 추가
-        const newTodo = {
-            id: makeNewId(),
-            text: $todoText.value,
-            done: false
-        };
-        todos.push(newTodo);
-        // console.log(newTodo);
-        // console.log(todos);
 
-        //2. 추가된 데이터를 화면에 렌더링
-        renderNewTodo(newTodo);
+    if (!validateInputText($todoText)) return
+    //1.데이터 처리: 필요한 데이터를 생성하여 todos배열에 추가
+    const newTodo = {
+        id: makeNewId(),
+        text: $todoText.value,
+        done: false
+    };
+    todos.push(newTodo);
+    // console.log(newTodo);
+    // console.log(todos);
 
-        //3.입력 완료 후 input창 텍스트 지우기
-        $todoText.value = '';
+    //2. 추가된 데이터를 화면에 렌더링
+    renderNewTodo(newTodo);
+
+    //3.입력 완료 후 input창 텍스트 지우기
+    $todoText.value = '';
 };
+
+//data-id값으로 배열을 탐색하여 아이디가 일치하는 객체의 인덱스를 반환
+function findIndexByDataId(dataId) {
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id === dataId) {
+            return i;
+
+        }
+    }
+    return null; //탐색 실패 증거로 null을 반환
+}
+//할 일 완료 처리 함수 정의
+function changeCheckState($label) {
+    /*
+        1.체크가 된 label태그에 checked클래스를 추가해야 함.
+        2.이 함수에는 체크가 된 label태그의 정보가 필요함
+        3.이 label태그는 이벤트 핸들러가 알고 있음
+    */
+    $label.classList.toggle('checked');
+    /* 
+        4.문제상황: CSS만 변했지 실제 데이터(done)는 변하지 않음.
+        5.만약 서버가 있었다면 서버에 완료상태를 반영해줘야 함
+        6.todos배열의 객체의 접근해서 done값을 변경해줘야 함
+        7.만약에 두번째 체크박스를 선택했다면 todos배열의 1번 인덱스의 
+        done값을 수정해야 한다.
+        8.label태그의 정보를 알고있다면 그 부모태그 li에 접근할 수 있고 
+        그 태그의 data-id속성을 조회하면 객체의 id값을 얻을 수 있음
+    */
+    // console.log($label.parentNode.dataset.id);
+    const dataId = +$label.parentNode.dataset.id;
+
+    /*
+    9. id값을 기반으로 배열을 탐색하여 data-id와 일치하는 
+    id 프로퍼티를 가진 객체가 있는 인덱스를 찾아야 함.
+    */
+    const idx = findIndexByDataId(dataId);
+    // console.log(`idx:${idx}번`);
+    //10.찾은 인덱스러 해당 객체에 접근해서 done값을 수정
+    if (idx !== null) {
+        todos[idx].done = !todos[idx].done;
+    }
+    // console.log(todos);
+};
+
+//할 일 삭제 처리 함수
+function removeToDoData($delTarget) {
+    //1. 삭제 대상 li태그를 ul에서 제거해야 함
+    const $ul = document.querySelector('.todo-list');
+    $ul.removeChild($delTarget)
+
+    //2. todos배열에서 해당 객체 삭제 
+    const delIdx = findIndexByDataId(+$delTarget.dataset.id);
+    todos.splice(delIdx, 1);
+    console.log(todos);
+}
+
+function renameMod() {
+    const $TodoList = document.querySelector('.todo-list .checkbox');
+    const $InputBox = document.createElement('input');
+
+    $TodoList.replaceChild($InputBox,$TodoList.lastElementChild)
+
+
+}
 
 
 //////////////////////////////
@@ -99,14 +168,38 @@ function insertTodoData() {
 
     $addBtn.addEventListener('click', e => {
         e.preventDefault(); //submit 전송기능 중단
-
         // console.log('추가 버튼 클릭');
 
-
-
         insertTodoData();
-
-
     });
+
+    //할 일 완료(체크박스) 이벤트
+    const $todoList = document.querySelector('.todo-list');
+    $todoList.addEventListener('change', e => {
+        // console.log('체크박스 체인지 이벤트 발생!');
+
+        // console.log(e.target.nextElementSibling);
+
+
+        // console.log(e.target.parentNode);
+        //체크박스에 변화가 생겼을 때 CSS변동 처리 함수 호출
+        changeCheckState(e.target.parentNode);
+    });
+    //할 일 삭제버튼 클릭 이벤트
+    $todoList.addEventListener('click', e => {
+
+        if (!e.target.matches('.remove span')) return;
+
+        // console.log('삭제버튼 클릭 됨 ',e.target);
+        // console.log(e.target.parentNode.parentNode);
+
+        removeToDoData(e.target.parentNode.parentNode);
+    })
+    $todoList.addEventListener('click', e => {
+        if (!e.target.matches('.modify span')) return;
+
+        //  console.log('수정 버튼 클릭 됨');
+        renameMod(e.target.parentNode);
+    })
 
 })();
